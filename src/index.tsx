734c99856b4dd319b1f7d98772f5255d5f8785b7
AkameTheCoder <joelkerbusch@gmail.com>
2023-08-02 02:54:46 +0200

    cleaning up

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React } from 'enmity/metro/common';
 import manifest from '../manifest.json';
-
-import Settings from './components/Settings';
 import { InformationCommands } from './commands';
 
 
@@ -17,9 +14,6 @@ const Version: Plugin = {
       this.commands = []
    },
 
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
-   }
 };
 
 registerPlugin(Version);
